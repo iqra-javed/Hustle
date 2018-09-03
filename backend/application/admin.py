cf1c781e2601f56b5dd8e9cbb19eb9239edf7239
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-09-02 23:08:37 -0500

    create superuser & register models with admin site

--- backend/application/admin.py
+++ backend/application/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Application, User_Application
 
-# Register your models here.
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ('company', 'position', 'created_on')
+    readonly_fields = ('created_on', 'last_modified')
+
+admin.site.register((Application, User_Application), ApplicationAdmin)
+
