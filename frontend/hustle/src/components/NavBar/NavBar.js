a36fd281a91fdbd5c9da19fa1f7d3daeb8e72df9
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-09-05 15:00:54 -0500

    create NavBar use material-ui/AppBar add some custom styles change component name Navigation -> NavBar add background image for app

---
+++ frontend/hustle/src/components/NavBar/NavBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, Button } from '@material-ui/core';
+
+import './NavBar.css';
+
+const NavBar = (props) => {
+    return (
+        <AppBar position="static">
+          <Toolbar style={{backgroundColor: 'rgba(212, 226, 14, 0.842)'}} className='nav-toolbar'>
+            <Typography variant="display2" color="inherit">
+              Hustle.
+            </Typography>
+            <Button 
+              style={{ fontSize: '1.5rem', backgroundColor: '#5c75e6' }} 
+              color="inherit" 
+              variant='contained' 
+              className='logout-btn'
+              >Logout
+            </Button>
+          </Toolbar>
+        </AppBar>
+    );
+  }
+
+  export default NavBar;
